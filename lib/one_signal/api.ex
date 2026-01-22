9779e5fa04695987282ac81cb609e3a8750763c5
Gustavo Viegas <webviegas@gmail.com>
2026-01-22 18:22:20 -0300

    Update basic working version to new elixir version

--- lib/one_signal/api.ex
+++ lib/one_signal/api.ex
@@ -2,38 +2,41 @@ defmodule OneSignal.API do
   @json_library OneSignal.json_library()
 
   def get(url, query \\ []) do
-    HTTPoison.start
+    HTTPoison.start()
     query = OneSignal.Utils.encode_body(query)
 
-    unless String.length(query) == 0 do
-      url = "#{url}?#{query}"
+    url = if String.length(query) == 0 do
+      url
+    else
+      "#{url}?#{query}"
     end
 
-    HTTPoison.get!(url, OneSignal.auth_header)
-    |> handle_response
+    HTTPoison.get!(url, OneSignal.auth_header())
+    |> handle_response()
   end
 
   def post(url, body) do
-    HTTPoison.start
+    HTTPoison.start()
 
     req_body = @json_library.encode!(body)
 
-    HTTPoison.post!(url, req_body, OneSignal.auth_header)
-    |> handle_response
+    HTTPoison.post!(url, req_body, OneSignal.auth_header())
+    |> handle_response()
   end
 
   def delete(url) do
-    HTTPoison.start
-    HTTPoison.delete!(url, OneSignal.auth_header)
-    |> handle_response
+    HTTPoison.start()
+
+    HTTPoison.delete!(url, OneSignal.auth_header())
+    |> handle_response()
   end
 
-  defp handle_response(%HTTPoison.Response{body: body, status_code: code}) when code in 200..299 do
+  defp handle_response(%HTTPoison.Response{body: body, status_code: code})
+       when code in 200..299 do
     {:ok, @json_library.decode!(body)}
   end
 
   defp handle_response(%HTTPoison.Response{body: body, status_code: _}) do
     {:error, @json_library.decode!(body)}
   end
-
 end
